c8fb38ee2b38ae60a850c29442d1153c1ef05697
Naofumi Ezaki <clmmns@gmail.com>
2015-04-23 13:51:59 -0400

    Test email done -Pprd

--- src/main/webapp/scripts/services.js
+++ src/main/webapp/scripts/services.js
@@ -252,6 +252,21 @@ ozayApp.factory('AuthenticationSharedService', function ($rootScope, $http, auth
             }
         };
     });
+    ozayApp.factory('NotificationService', function ($resource) {
+            return $resource('api/notifications/:id', {}, {
+                        'query': { method: 'GET', isArray: true},
+                        'get': {
+                            method: 'GET',
+                            transformResponse: function (data) {
+                                data = angular.fromJson(data);
+                                data.issueDate = new Date(data.issueDate);
+                                data.createdDate = new Date(data.createdDate);
+                                return data;
+                            }
+                        }
+                    });
+        });
+
 
 
 
